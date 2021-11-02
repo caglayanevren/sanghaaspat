9dc96e9b5569aee5ea589be81a9dbcf92f61165e
Evren Çağlayan <caglayanevren@gmail.com>
2021-11-03 00:13:36 +0300

    remove console logs

--- pages/sangha.js
+++ pages/sangha.js
@@ -29,7 +29,7 @@ export async function getStaticProps({ locale }) {
 export default function About(props) {
     return (
         <Layout>
-            {console.log(props.results)}
+            {/* {console.log(props.results)} */}
             <Head>
                 <title>
                     {props.locale === 'en'
