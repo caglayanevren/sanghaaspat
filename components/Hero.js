76f4de2d1111d2f9aa6a0f71fb7bdd5d0b7d11f1
Evren Çağlayan <caglayanevren@gmail.com>
2021-11-08 13:09:57 +0300

    Hero background image optimization

--- components/Hero.js
+++ components/Hero.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { Link, Icon, Stack, Flex, VStack, Box } from '@chakra-ui/react';
 import { HiChevronDoubleDown } from 'react-icons/hi';
 import Sanghalogo from './Sanghalogo';
@@ -8,48 +9,58 @@ export default function Hero({ motto }) {
         <Flex
             w={'full'}
             h={{ base: '100vh', md: 'calc(100vh - 88px)' }}
-            backgroundImage={'url(hero.jpg)'}
-            backgroundSize={'cover'}
-            backgroundPosition={'20% 50%'}
+            //backgroundImage={'url(hero.jpg)'}
+            //backgroundSize={'cover'}
+            //backgroundPosition={'20% 50%'}
         >
-            <VStack
-                id="evrentest"
-                w={'full'}
-                bgGradient={'linear(to-r, blackAlpha.600, blackAlpha.600)'}
-                className={blur}
-            >
+            <VStack w={'full'} position="relative">
+                <Image
+                    src="/hero.jpg"
+                    alt="hero"
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"
+                    priority={true}
+                />
                 <VStack
-                    justify={'space-between'}
+                    id="evrentest"
                     w={'full'}
-                    h={'100%'}
-                    id="evrentest2"
-                    zIndex={'2'}
+                    bgGradient={'linear(to-r, blackAlpha.600, blackAlpha.600)'}
+                    className={blur}
                 >
-                    <Box>&nbsp;</Box>
-                    <Stack align={'center'} marginTop={0}>
-                        <Box
-                            width={{ base: '250px', md: '350px' }}
-                            height={{ base: '250px', md: '350px' }}
-                        >
-                            <Sanghalogo />
-                        </Box>
-                        <Box
-                            as="h1"
-                            fontSize={{
-                                base: '1.5rem',
-                                sm: '2rem',
-                                md: '2.25rem',
-                            }}
-                            marginTop={16}
-                            mt={5}
-                            textAlign={'center'}
-                        >
-                            {motto}
-                        </Box>
-                    </Stack>
-                    <Link to={'#'}>
-                        <Icon boxSize={'2rem'} as={HiChevronDoubleDown} />
-                    </Link>
+                    <VStack
+                        justify={'space-between'}
+                        w={'full'}
+                        h={'100%'}
+                        id="evrentest2"
+                        zIndex={'2'}
+                    >
+                        <Box>&nbsp;</Box>
+                        <Stack align={'center'} marginTop={0}>
+                            <Box
+                                width={{ base: '250px', md: '350px' }}
+                                height={{ base: '250px', md: '350px' }}
+                            >
+                                <Sanghalogo />
+                            </Box>
+                            <Box
+                                as="h1"
+                                fontSize={{
+                                    base: '1.5rem',
+                                    sm: '2rem',
+                                    md: '2.25rem',
+                                }}
+                                marginTop={16}
+                                mt={5}
+                                textAlign={'center'}
+                            >
+                                {motto}
+                            </Box>
+                        </Stack>
+                        <Link to={'#'}>
+                            <Icon boxSize={'2rem'} as={HiChevronDoubleDown} />
+                        </Link>
+                    </VStack>
                 </VStack>
             </VStack>
         </Flex>
