d3157ba7465c000fa83c3ca3976a53c9f2f43110
Evren Çağlayan <caglayanevren@gmail.com>
2022-01-26 09:17:28 +0300

    add content to Reviews page

--- pages/events.js
+++ pages/events.js
@@ -27,28 +27,6 @@ import firstImage from '../public/images/events/events.jpg';
 import en from '../locales/en';
 import tr from '../locales/tr';
 
-/* export async function getStaticProps({ locale }) {
-    const notion = new Client({ auth: process.env.NOTION_API_KEY });
-    const pageIdEn = '77c9eaae32e24958aaf3650265dac47a';
-    const pageIdTr = '2a209e6b6d5d4400954c5e8512ba8b56';
-
-    const pageId =
-        locale === 'en' ? pageIdEn : locale === 'tr' ? pageIdTr : 'lang error';
-
-    const response = await notion.blocks.children.list({
-        block_id: pageId,
-        page_size: 50,
-    });
-    return {
-        props: {
-            results: response,
-            locale,
-            pageId,
-        },
-        revalidate: 30,
-    };
-} */
-
 export default function Events(props) {
     const router = useRouter();
     const { locale } = router;
