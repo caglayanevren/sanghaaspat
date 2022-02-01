3fbe333ff36097122c3261f0704945bba96ff984
Evren Çağlayan <caglayanevren@gmail.com>
2022-02-01 23:56:40 +0300

    Flash Of Unstyled Text problem corrected real

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -3,9 +3,6 @@ import Head from 'next/head';
 export default function CustomHead({ pageName, locale, ...props }) {
     return (
         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;900&amp;display=swap" rel="stylesheet"/>
             <title>
                 {locale == 'en'
                     ? `${process.env.siteTitle} | ${pageName.english.title}`
