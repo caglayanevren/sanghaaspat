bc53d7808081fe9d105b1704ddc803c94695d70a
Evren Çağlayan <caglayanevren@gmail.com>
2022-02-02 18:53:38 +0300

    crossOrigin true

--- pages/_document.js
+++ pages/_document.js
@@ -19,56 +19,56 @@ class MyDocument extends Document {
                         href="/_next/static/media/raleway-all-300-normal.59104fc9.woff"
                         as="font"
                         type="font/woff"
-                        crossOrigin=""
+                        crossOrigin="true"
                     />
                     <link
                         rel="preload"
                         href="/_next/static/media/raleway-all-400-normal.beb67d10.woff"
                         as="font"
                         type="font/woff"
-                        crossOrigin=""
+                        crossOrigin="true"
                     />
                     <link
                         rel="preload"
                         href="/_next/static/media/raleway-all-600-normal.3153ae14.woff"
                         as="font"
                         type="font/woff"
-                        crossOrigin=""
+                        crossOrigin="true"
                     />
                     <link
                         rel="preload"
                         href="/_next/static/media/raleway-all-900-normal.96761567.woff"
                         as="font"
                         type="font/woff"
-                        crossOrigin=""
+                        crossOrigin="true"
                     />
                     <link
                         rel="preload"
                         href="/_next/static/media/raleway-latin-300-normal.8c5f9d02.woff2"
                         as="font"
                         type="font/woff2"
-                        crossOrigin=""
+                        crossOrigin="true"
                     />
                     <link
                         rel="preload"
                         href="/_next/static/media/raleway-latin-400-normal.7275827e.woff2"
                         as="font"
                         type="font/woff2"
-                        crossOrigin=""
+                        crossOrigin="true"
                     />
                     <link
                         rel="preload"
                         href="/_next/static/media/raleway-latin-600-normal.1f652937.woff2"
                         as="font"
                         type="font/woff2"
-                        crossOrigin=""
+                        crossOrigin="true"
                     />
                     <link
                         rel="preload"
                         href="/_next/static/media/raleway-latin-900-normal.cc903836.woff2"
                         as="font"
                         type="font/woff2"
-                        crossOrigin=""
+                        crossOrigin="true"
                     />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;900&amp;display=optional"
