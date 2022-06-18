9aab2ba24a14e6ba0f596f4c308267cce94a7ef6
Evren Çağlayan <caglayanevren@gmail.com>
2022-06-18 13:27:15 +0300

    add some links label

--- components/navbar/MenuLinks.js
+++ components/navbar/MenuLinks.js
@@ -10,44 +10,23 @@ export default function MenuLinks({ isOpen }) {
     const router = useRouter();
     const { locale, locales, defaultLocale } = router;
     return (
-        <Box
-            display={{ base: isOpen ? 'block' : 'none', md: 'block' }}
-            flexBasis={{ base: '100%', md: 'auto' }}
-        >
-            <Stack
-                spacing={[8, 8, 7, 10]}
-                align="center"
-                justify={['center', 'space-between', 'flex-end', 'flex-end']}
-                direction={['column', 'column', 'row', 'row']}
-                pt={[4, 4, 0, 0]}
-            >
-                <Link
-                    href={
-                        locale === 'en'
-                            ? '/'
-                            : locale === 'tr'
-                            ? '/tr/'
-                            : 'lang error'
-                    }
-                >
+        <Box display={{ base: isOpen ? 'block' : 'none', md: 'block' }} flexBasis={{ base: '100%', md: 'auto' }}>
+            <Stack spacing={[8, 8, 7, 10]} align="center" justify={['center', 'space-between', 'flex-end', 'flex-end']} direction={['column', 'column', 'row', 'row']} pt={[4, 4, 0, 0]}>
+                <Link href={locale === 'en' ? '/' : locale === 'tr' ? '/tr/' : 'lang error'} aria-label={locale === 'en' ? process.env.home.english.title : locale === 'tr' ? process.env.home.turkish.title : 'lang error'}>
                     <SunIcon />
                 </Link>
                 <MenuItem
                     to="/about-sangha"
                     toTr="/sangha-hakkinda"
                     textEn={
-                        <div
-                            style={{ lineHeight: '1.25', textAlign: 'center' }}
-                        >
+                        <div style={{ lineHeight: '1.25', textAlign: 'center' }}>
                             About
                             <br />
                             Sangha
                         </div>
                     }
                     textTr={
-                        <div
-                            style={{ lineHeight: '1.25', textAlign: 'center' }}
-                        >
+                        <div style={{ lineHeight: '1.25', textAlign: 'center' }}>
                             Sangha
                             <br />
                             Hakkında
@@ -58,60 +37,30 @@ export default function MenuLinks({ isOpen }) {
                     to="/qigong-classes"
                     toTr="/qigong-dersleri"
                     textEn={
-                        <div
-                            style={{ lineHeight: '1.25', textAlign: 'center' }}
-                        >
+                        <div style={{ lineHeight: '1.25', textAlign: 'center' }}>
                             Qi&nbsp;Gong
                             <br />
                             Classes
                         </div>
                     }
                     textTr={
-                        <div
-                            style={{ lineHeight: '1.25', textAlign: 'center' }}
-                        >
+                        <div style={{ lineHeight: '1.25', textAlign: 'center' }}>
                             Qi&nbsp;Gong
                             <br />
                             Dersleri
                         </div>
                     }
                 />
-                <MenuItem
-                    to="/qimassage"
-                    toTr="/qimasaj"
-                    textEn="Qi&nbsp;Massage"
-                    textTr="Qi&nbsp;Masaj"
-                />
-                <MenuItem
-                    to="/events"
-                    toTr="/etkinlikler"
-                    textEn="Events"
-                    textTr="Etkinlikler"
-                />
-                <MenuItem
-                    to="/reviews"
-                    toTr="/yorumlar"
-                    textEn="Reviews"
-                    textTr="Yorumlar"
-                />
-                <MenuItem
-                    to="/contact"
-                    toTr="/iletisim"
-                    textEn="Contact"
-                    textTr="İletişim"
-                />
+                <MenuItem to="/qimassage" toTr="/qimasaj" textEn="Qi&nbsp;Massage" textTr="Qi&nbsp;Masaj" />
+                <MenuItem to="/events" toTr="/etkinlikler" textEn="Events" textTr="Etkinlikler" />
+                <MenuItem to="/reviews" toTr="/yorumlar" textEn="Reviews" textTr="Yorumlar" />
+                <MenuItem to="/contact" toTr="/iletisim" textEn="Contact" textTr="İletişim" />
                 {/* <ChangeColorMode /> */}
                 <Stack direction={'row'}>
-                    <Link
-                        href="https://www.facebook.com/bodrumqigong"
-                        isExternal
-                    >
+                    <Link href="https://www.facebook.com/bodrumqigong" isExternal aria-label="facebook">
                         <Icon as={FaFacebook} />
                     </Link>
-                    <Link
-                        href="https://www.instagram.com/sanghaaspat/"
-                        isExternal
-                    >
+                    <Link href="https://www.instagram.com/sanghaaspat/" isExternal aria-label="instagram">
                         <Icon as={FaInstagram} />
                     </Link>
                 </Stack>
