e6172f6a58b1cf0d71d9b4985d3765ef89773873
Evren Caglayan <caglayanevren@gmail.com>
2022-06-22 12:06:50 +0300

    change preload fonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-                    />
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `
@@ -19,75 +16,10 @@ class MyDocument extends Document {
                         `,
                         }}
                     />
-                    <link
-                        rel="preconnect"
-                        href="https://fonts.googleapis.com"
-                    />
-                    <link
-                        rel="preconnect"
-                        href="https://fonts.gstatic.com"
-                        crossOrigin="true"
-                    />
-                    <link
-                        rel="preload"
-                        href="/_next/static/media/raleway-all-300-normal.59104fc9.woff"
-                        as="font"
-                        type="font/woff"
-                        crossOrigin="true"
-                    />
-                    <link
-                        rel="preload"
-                        href="/_next/static/media/raleway-all-400-normal.beb67d10.woff"
-                        as="font"
-                        type="font/woff"
-                        crossOrigin="true"
-                    />
-                    <link
-                        rel="preload"
-                        href="/_next/static/media/raleway-all-600-normal.3153ae14.woff"
-                        as="font"
-                        type="font/woff"
-                        crossOrigin="true"
-                    />
-                    <link
-                        rel="preload"
-                        href="/_next/static/media/raleway-all-900-normal.96761567.woff"
-                        as="font"
-                        type="font/woff"
-                        crossOrigin="true"
-                    />
-                    <link
-                        rel="preload"
-                        href="/_next/static/media/raleway-latin-300-normal.8c5f9d02.woff2"
-                        as="font"
-                        type="font/woff2"
-                        crossOrigin="true"
-                    />
-                    <link
-                        rel="preload"
-                        href="/_next/static/media/raleway-latin-400-normal.7275827e.woff2"
-                        as="font"
-                        type="font/woff2"
-                        crossOrigin="true"
-                    />
-                    <link
-                        rel="preload"
-                        href="/_next/static/media/raleway-latin-600-normal.1f652937.woff2"
-                        as="font"
-                        type="font/woff2"
-                        crossOrigin="true"
-                    />
-                    <link
-                        rel="preload"
-                        href="/_next/static/media/raleway-latin-900-normal.cc903836.woff2"
-                        as="font"
-                        type="font/woff2"
-                        crossOrigin="true"
-                    />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;900&amp;display=optional"
-                        rel="stylesheet"
-                    />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link rel="preload" href="https://fonts.gstatic.com/s/raleway/v27/1Ptug8zYS_SKggPNyC0IT4ttDfA.woff2" as="font" type="font/woff2" crossOrigin="true" />
+                    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;900&amp;display=optional" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
