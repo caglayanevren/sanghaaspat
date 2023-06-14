9c9a89ad814d7f3d28b95daa887fce0019e86f2d
Evren Çağlayan <caglayanevren@users.noreply.github.com>
2023-06-14 13:01:30 +0300

    changed: hero background image

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -8,33 +8,16 @@ export default function Hero({ motto }) {
     return (
         <Flex w={'full'} h={{ base: '100vh', md: 'calc(100vh - 88px)' }}>
             <VStack w={'full'} position="relative">
-                <Image
-                    src="/images/home/hero_v3.jpg"
-                    alt="Sangha Aspat"
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="center"
-                    priority={true}
-                    as="image"
-                />
+                <Image src="/images/home/hero_v4.jpg" alt="Sangha Aspat" layout="fill" objectFit="cover" objectPosition="center" priority={true} as="image" />
                 <VStack
                     w={'full'}
                     //bgGradient={'linear(to-r, blackAlpha.600, blackAlpha.600)'}
                     className={blur}
                 >
-                    <VStack
-                        justify={'space-between'}
-                        w={'full'}
-                        h={'100%'}
-                        zIndex={'2'}
-                        spacing="0"
-                    >
+                    <VStack justify={'space-between'} w={'full'} h={'100%'} zIndex={'2'} spacing="0">
                         <Box>&nbsp;</Box>
                         <VStack align={'center'} spacing="50px">
-                            <Box
-                                width={{ base: '250px', md: '300px' }}
-                                height={{ base: '250px', md: '300px' }}
-                            >
+                            <Box width={{ base: '250px', md: '300px' }} height={{ base: '250px', md: '300px' }}>
                                 <Sanghalogo />
                             </Box>
                             <Box
@@ -50,12 +33,7 @@ export default function Hero({ motto }) {
                             </Box>
                         </VStack>
                         <Link href="#sangha" aria-label="Down">
-                            <Icon
-                                className={bounceicon}
-                                marginBottom={4}
-                                boxSize={'2rem'}
-                                as={HiChevronDoubleDown}
-                            />
+                            <Icon className={bounceicon} marginBottom={4} boxSize={'2rem'} as={HiChevronDoubleDown} />
                         </Link>
                     </VStack>
                 </VStack>
