dcf46d7f96c3bc94da313f748c1c860444a2b5c5
Evren Çağlayan <caglayanevren@users.noreply.github.com>
2023-06-20 19:17:02 +0300

    raleway font revision

--- pages/_document.js
+++ pages/_document.js
@@ -16,10 +16,10 @@ class MyDocument extends Document {
                         `,
                         }}
                     />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyCMIT4ttDfCmxA.woff2" as="font" type="font/woff2" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;900&amp;display=optional" rel="stylesheet" />
+                    {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
+                    {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> */}
+                    {/* <link rel="preconnect" href="https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyCMIT4ttDfCmxA.woff2" as="font" type="font/woff2" crossOrigin="true" /> */}
+                    {/* <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;900&amp;display=optional" rel="stylesheet" /> */}
                     <meta name="google" content="notranslate" />
                 </Head>
                 <body>
