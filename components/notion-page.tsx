4963724a2d3dfd4917f7e4fe1f31720f267aaf4e
Evren Çağlayan <caglayanevren@users.noreply.github.com>
2023-07-19 17:13:20 +0300

    changed: fix inline images, added: mapImageUrl function

--- components/notion-page.tsx
+++ components/notion-page.tsx
@@ -2,10 +2,9 @@
 
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
-import Link from 'next/link';
 
 import { useTheme } from 'next-themes';
-import { ExtendedRecordMap } from 'notion-types';
+import { Block, ExtendedRecordMap } from 'notion-types';
 import { NotionRenderer } from 'react-notion-x';
 
 import CategoryList from '@/components/category-list';
@@ -31,13 +30,13 @@ export default function NotionPage({ post, recordMap }: { post: Post; recordMap:
                     <CategoryList categories={post.categories} />
                 </div>
             }
+            mapImageUrl={(url, block) => mapImageUrl(url, block) || ''}
             components={{
                 Code,
                 Collection,
                 Equation,
                 Modal,
                 Pdf,
-                nextLink: Link,
                 nextImage: Image,
             }}
         />
@@ -53,3 +52,57 @@ const Pdf = dynamic(() => import('react-notion-x/build/third-party/pdf').then((m
 const Modal = dynamic(() => import('react-notion-x/build/third-party/modal').then((m) => m.Modal), {
     ssr: false,
 });
+
+export function mapImageUrl(url: string, block: Block): string | null {
+    if (!url) {
+        return null;
+    }
+
+    if (url.startsWith('data:')) {
+        return url;
+    }
+
+    // more recent versions of notion don't proxy unsplash images
+    if (url.startsWith('https://images.unsplash.com')) {
+        return url;
+    }
+
+    try {
+        const u = new URL(url);
+
+        if (
+            u.pathname.startsWith('/secure.notion-static.com') &&
+            u.hostname.endsWith('.amazonaws.com')
+        ) {
+            if (
+                u.searchParams.has('X-Amz-Credential') &&
+                u.searchParams.has('X-Amz-Signature') &&
+                u.searchParams.has('X-Amz-Algorithm')
+            ) {
+                // if the URL is already signed, then use it as-is
+                return url;
+            }
+        }
+    } catch {
+        // ignore invalid urls
+    }
+
+    if (url.startsWith('/images')) {
+        url = `https://www.notion.so${url}`;
+    }
+
+    url = `https://www.notion.so${url.startsWith('/image') ? url : `/image/${encodeURIComponent(url)}`}`;
+
+    const notionImageUrlV2 = new URL(url);
+    let table = block.parent_table === 'space' ? 'block' : block.parent_table;
+    if (table === 'collection' || table === 'team') {
+        table = 'block';
+    }
+    notionImageUrlV2.searchParams.set('table', table);
+    notionImageUrlV2.searchParams.set('id', block.id);
+    notionImageUrlV2.searchParams.set('cache', 'v2');
+
+    url = notionImageUrlV2.toString();
+
+    return url;
+}
--- next.config.js
+++ next.config.js
@@ -17,6 +17,10 @@ module.exports = {
                 protocol: 'https',
                 hostname: 'www.notion.so',
             },
+            {
+                protocol: 'https',
+                hostname: 's3-us-west-2.amazonaws.com',
+            },
         ],
     },
     trailingSlash: true,
